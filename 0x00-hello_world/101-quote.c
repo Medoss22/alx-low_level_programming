c1e7de69b6ac1f0b2b55741bc3e39349a6502448
Medoss22 <medm82050@gmail.com>
2023-06-15 21:01:54 +0100

    a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
-#include <string.h>
-
-int main() 
+/**
+* main - Main entry point description
+* Return: Always 1 (Success)
+*/
+int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, strlen(message));
-return 1;
+char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, m, 59);
+return (1);
 }
