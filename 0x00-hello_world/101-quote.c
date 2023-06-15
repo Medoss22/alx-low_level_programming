b574427b00b17dad9d63b5401364daff999e1b73
Medoss22 <medm82050@gmail.com>
2023-06-15 19:17:21 +0100

    a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 #include <string.h>
 
-int main() {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, strlen(message));
-    return 1;
+int main() 
+{
+const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, strlen(message));
+return 1;
 }
