1c7857f72056376be259279387ae48222af24515
Medoss22 <medm82050@gmail.com>
2023-06-16 22:34:24 +0100

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main -Enter prenter
+* description : print all the alphabet 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	chart let = 'a';
+	while (let <='z')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar('\n');
+	return (0);
+}
