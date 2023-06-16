e165b6adb0221d295fa76161a0c9bad1e52e9d40
Medoss22 <medm82050@gmail.com>
2023-06-16 23:12:53 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Prints
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char let = 'a';
+
+	while (let <= 'z')
+	{
+		putchar(let);
+		let++;
+	}
+
+	let = 'A';
+
+	while (let <= 'Z')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar('\n');
+
+	return(0);
+}
