fc638e567f3868cd41089f3b3dc58d4aeed1e966
Medoss22 <medm82050@gmail.com>
2023-06-16 19:51:16 +0100

    Positive is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,19 +17,19 @@ int main(void)
 	srand(time(0));
 	
 	n= rand();
-	printf("The num is %d\n", n);
+	printf("The number is %d\n", n);
 
 	if (n > 0) 
 	{
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
 	} 
 	else if (n == 0) 
 	{
-		printf("%d is zero\n");
+		printf("%d is zero\n", n);
 	} 
 	else 
 	{
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 
 	return(0);
