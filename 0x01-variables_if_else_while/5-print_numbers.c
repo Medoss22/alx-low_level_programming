136c7f756148b9a06190f4b87110f1599fa91543
Medoss22 <medm82050@gmail.com>
2023-06-16 23:36:02 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** 
+* main - Prints num from 0 to 9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int N = 0;
+
+	while (N < 10)
+	{
+		putchar(N + '0');
+		N++;
+	}
+	putchar('\n');
+	return (0);
+}
