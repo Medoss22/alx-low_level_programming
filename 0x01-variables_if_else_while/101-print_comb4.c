e108eae27821835ba7e6df61a624b6648b5647b3
Medoss22 <medm82050@gmail.com>
2023-06-19 10:35:50 +0100

    The success combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry poiter
+*
+* Description all possible different combinations of three digits
+* Return: Alwyas 0 (Success)
+*/
+
+int mani(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a +1; b <= 8; b++)
+		{
+			for (c = b +1; c <= 9; c++)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar('0' + C);
+				if ( a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	returne (0);
+}
