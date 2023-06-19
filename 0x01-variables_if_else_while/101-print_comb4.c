0969c15d066459f93de262325ce75f0907167e64
Medoss22 <medm82050@gmail.com>
2023-06-19 10:46:11 +0100

    The success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (a = 0; a <= 7; a++)
 	{
-		for (b = a +1; b <= 8; b++)
+		for (b = a + 1; b <= 8; b++)
 		{
-			for (c = b +1; c <= 9; c++)
+			for (c = b + 1; c <= 9; c++)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
 				putchar('0' + c);
-				if ( a != 7 || b != 8 || c != 9)
+				if (a != 7 || b != 8 || c != 9)
 				{
 					putchar(',');
 					putchar(' ');
