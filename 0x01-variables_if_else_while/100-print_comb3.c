64c9df175c44db78376ee07411a47a6a55e41d69
Medoss22 <medm82050@gmail.com>
2023-06-19 10:16:34 +0100

    The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - pronter
+* Decription : prints all possible different combinations of two digits
+* Return: Alwyas 0 (Success)
+*/
+
+int main(void)
+{
+	int i, h;
+	for (i = 0; i <= 8; i++)
+	{
+		for (h = i + 1; h <= 9; h++)
+		{
+			putchar('0' + i);
+			putchar('0' + h);
+			if (i != 8 || h != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\m');
+	return (0);
+}
