0310c6809c34a87377a6b368cd8906e0e9b5713c
Medoss22 <medm82050@gmail.com>
2023-06-20 17:16:18 +0100

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 * main - printer
 *
-* print_alphabet - prints the alphabe, in lowercase
+* print_alphabet - Prints the alphabet in lowercase followed by a new line.
 */
 
 void print_alphabet(void)
