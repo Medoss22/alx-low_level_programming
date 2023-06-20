d4d6ec6e9bbca8b4f16487d05183a4e266377121
Medoss22 <medm82050@gmail.com>
2023-06-20 23:01:03 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  *  _isalpha - a function that checks for alphabetic character.
  *  @c: is the character to be checked
- *  Return: 1 is c is a letter, lowercase or uppercase and 0 otherwise
+ *  Return: 1 is x is a letter, lowercase or uppercase and 0 otherwise
  */
 int _isalpha(int x)
 {
