456acc7175fb0a3c223cba4582eb0e6ea11e37a5
Medoss22 <medm82050@gmail.com>
2023-06-20 13:41:45 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * main - printer
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
 	return (0);
 }
