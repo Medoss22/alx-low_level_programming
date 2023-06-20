c3a9bb57aba5a9869878a4218e41db752752db2b
Medoss22 <medm82050@gmail.com>
2023-06-20 17:01:12 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,6 @@
 
 int main(void)
 {
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
