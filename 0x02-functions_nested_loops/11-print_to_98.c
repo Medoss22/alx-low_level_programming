c2934acc4c9e4643a3b89184a9e24078b9b5dfde
Medoss22 <medm82050@gmail.com>
2023-06-21 16:17:52 +0100

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,27 +9,29 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (int i = n; i <= 98; i++)
-        {
-            printf("%d", i);
-            if (i != 98)
-            {
-                printf(", ");
-            }
-        }
-    }
-    else
-    {
-        for (int i = n; i >= 98; i--)
-        {
-            printf("%d", i);
-            if (i != 98)
-            {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+			printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (int i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
 }
