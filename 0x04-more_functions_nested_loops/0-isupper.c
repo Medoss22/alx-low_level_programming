fddc7239e3334b3edbcd8520b0ab310b8b86f11d
Medoss22 <medm82050@gmail.com>
2023-06-22 23:53:37 +0100

    1-commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Suara Ayomide
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @char: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int char)
+{
+	if (char >= 'A' && char <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+]
