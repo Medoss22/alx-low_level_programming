dac3b9578154ce92975fc7235cde2efe25fba92f
Medoss22 <medm82050@gmail.com>
2023-06-23 22:13:10 +0100

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
