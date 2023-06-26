175fbb6cd1ae5e8349aad2f943831f185a699c97
Medoss22 <medm82050@gmail.com>
2023-06-26 21:58:54 +0100

    I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: The input string.
+ */
+void _puts(char *str)
+{
+	int y + 0;
+
+	while (srt[y] != '\0')
+	{
+		_putchar(str[y]);
+		y++;
+	}
+	_putchar('\n');
+}
