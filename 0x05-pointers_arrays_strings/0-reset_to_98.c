4d2d31503a371f70d19833617fd7906f3d9abe56
Medoss22 <medm82050@gmail.com>
2023-06-26 19:36:08 +0100

    1er commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,10 @@
 * reset_to_98 - Updates the value pointed to by n to 98
 *
 * @n: Pointer to an int.
-* Descrition: This function takes a pointer to an int as a parameter and updates the value it points to 98
+* Descrition: This function takes a pointer to an int as a parameter and updates
+* the value it points to 98
 */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	putchar(*n);
 }
