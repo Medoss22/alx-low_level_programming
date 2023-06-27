1b0c469da58215555fc79d3d7365772ba44677bf
Medoss22 <medm82050@gmail.com>
2023-06-27 10:34:10 +0100

    size

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string.
+ *@str: pointer to string.
+ *Return: No return.
+ */
+void puts2(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+		if (s % 2 ==0)
+			_putchar(str[s]);
+		s++;
+	}
+	_putchar('\n');
+}
