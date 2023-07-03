4a2106697f241fcd5ee8ccd1c654c33add910d5f
Medoss22 <medm82050@gmail.com>
2023-07-03 22:29:51 +0100

    final commt

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ *@s: pr to string.
+ *@accept: pr to string.
+ *Return: return (s).
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0 ; *(accept + a) ; a++)
+		{
+			if (*s == *(accept + a))
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
