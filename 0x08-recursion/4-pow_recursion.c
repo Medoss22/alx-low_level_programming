ca3d44db5f2dbcb37b2505820c270ae2b173bf4f
Medoss22 <medm82050@gmail.com>
2023-07-05 20:47:11 +0100

    5th commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y recursively
+ * @x: The base number
+ * @y: The exponent
+ * Return: The value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
