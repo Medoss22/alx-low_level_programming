7838956ed7b46a17f8b816260a196e499c67515f
Medoss22 <medm82050@gmail.com>
2023-07-05 13:07:54 +0100

    1st commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _puts_recursion - Prints a string recursively
+ * @s: Pointer to the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
