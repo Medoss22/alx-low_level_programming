6df8cba5332ab3d6e6bc0a6576cf68cd6a9aee02
Medoss22 <medm82050@gmail.com>
2023-07-07 22:30:55 +0100

    2ed commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments passed
+ * @argv: An array of strings containing the command-line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
