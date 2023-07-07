649f08e0cfa57388dc0bd3c166d14a2eabe58cfa
Medoss22 <medm82050@gmail.com>
2023-07-07 23:06:50 +0100

    3ed commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int r;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int r;
-
-	r = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", r);
-	return (0);
+	else
+	{
+		r = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", r);
+		return (0);
+	}
 }
