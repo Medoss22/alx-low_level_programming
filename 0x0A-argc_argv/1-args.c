6d84c4574eab0a0023df3330f1222b45b606272c
Medoss22 <medm82050@gmail.com>
2023-07-07 22:25:19 +0100

    2ed commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: An array of strings containing the command-line arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
