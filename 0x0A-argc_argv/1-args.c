87c3dfb039483fe310ccc06edec3c7aa6487b967
Medoss22 <medm82050@gmail.com>
2023-07-07 22:21:42 +0100

    2ed commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments passed
+ * @argv: An array of strings containing the command-line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
