e6f037104b1e5ce7812d1d3ba91fb494967bbefa
Medoss22 <medm82050@gmail.com>
2023-07-10 11:16:49 +0100

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	char *array = (char *)malloc(size * sizeof(char));
+	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
