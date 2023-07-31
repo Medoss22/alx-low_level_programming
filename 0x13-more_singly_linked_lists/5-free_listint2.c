283fa9e820f1b99b66d8c3de5fdeb49da215ef2a
Medoss22 <medm82050@gmail.com>
2023-07-31 22:35:45 +0100

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 	listint_t *c;
 
 	if (head == NULL)
-	return ();
-
-	while (*head != NULL)
 	{
-		c = *head;
-		*head = (*head)->next;
-		free(c);
+		while (*head != NULL)
+		{
+			c = *head;
+			*head = (*head)->next;
+			free(c);
+		}
 	}
 }
