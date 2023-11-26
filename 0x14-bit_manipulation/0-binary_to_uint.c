5b7638d675d0cd879a5cd642e515c62c118e1823
Medoss22 <medm82050@gmail.com>
2023-11-26 14:13:14 +0100

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b) {
-	if (b == NULL)
-		return 0;
+/**
+ *binary_to_uint - a function that converts a binary number to an unsigned int.
+ *@b: is pointing to a string of 0 and 1 chars
+ *Return: the converted number, or 0 if.
+*/
 
-	unsigned int result = 0;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int conv_num = 0, len = 0;
 
-	while (*b != '\0') {
-		if (*b == '0' || *b == '1') {
-			result = result * 2 + (*b - '0');
-			b++;
-		} else {
-			return 0;
-		}
+	if (!b)
+		return (0);
+
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		conv_num = conv_num << 1;
+		if (b[len++] & 1)
+			conv_num += 1;
 	}
-	return result;
-}
-
-int main() {
-	const char *binary_str = "110101";
-	unsigned int result = binary_to_uint(binary_str);
-
-	if (result == 0 && binary_str[0] != '0') {
-		printf("Invalid binary string\n");
-	} else {
-		printf("Decimal equivalent of binary %s is %u\n", binary_str, result);
-	}
-	return 0;
+	return (conv_num);
 }
